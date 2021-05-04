89c190324349f4556eba1c8512031e0363377a93
shardul <surmashardul@gmail.com>
2021-05-04 14:13:27 +0530

    demo-deploy

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/bundle.js',
+                '/index.html',
+                '/',
+            ])
+        })
+    )
+})
+this.addEventListener("fetch", (event) => {
+
+
+    if (!navigator.onLine) {
+        if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
+            event.waitUntil(
+                this.registration.showNotification("Internet", {
+                    body: "internet not working",
+                })
+            )
+        }
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+                let requestUrl = event.request.clone();
+                fetch(requestUrl)
+            })
+        )
+    }
+}) 
