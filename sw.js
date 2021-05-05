971e98841dd671df0fd4f693297d3f7c05e01622
shardul <surmashardul@gmail.com>
2021-05-05 14:51:35 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,35 +1,42 @@
-const cacheData = "appV1";
-this.addEventListener("install", (event) => {
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'offline.html' ];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheData).then((cache) => {
-            cache.addAll([
-                '/static/js/main.chunk.js',
-                '/static/js/bundle.js',
-                '/index.html',
-                '/',
-            ])
-        })
-    )
-})
-this.addEventListener("fetch", (event) => {
-
-
-    if (!navigator.onLine) {
-        if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
-            event.waitUntil(
-                this.registration.showNotification("Internet", {
-                    body: "internet not working",
-                })
-            )
-        }
-        event.respondWith(
-            caches.match(event.request).then((resp) => {
-                if (resp) {
-                    return resp
-                }
-                let requestUrl = event.request.clone();
-                fetch(requestUrl)
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                 cache.addAll(urlsToCache);
             })
-        )
-    }
-}) 
+    )
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+     event.respondWith(
+     caches.match(event.request)
+         .then(() => {
+             return fetch(event.request)
+             .catch(() => caches.match('offline.html'))
+         })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
